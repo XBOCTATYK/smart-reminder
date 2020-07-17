e036fcfd2051f1320725732f439edfcaee3cb97c
XBOCTATYK <sha-ej@yandex.ru>
2020-07-17 23:35:13 +0300

    dont create unused notifications

--- src/utils/createNextNotification.ts
+++ src/utils/createNextNotification.ts
@@ -3,13 +3,16 @@ import { getNextNotifyTime } from './calculateTime';
 export async function createNextNotification(DB, task) {
     try {
         const id = task.id;
-        await DB.model('Tasks').update({ ...task, notificationsDone: task.notificationsDone + 1 }, { where: { id } })
+        const notificationsDone = task.notificationsDone + 1;
+        await DB.model('Tasks').update({ ...task, notificationsDone }, { where: { id } })
 
-        const DBUserResponse = await DB.model('User').findOne({ where: { id: task.user_id } });
-        const user = DBUserResponse.dataValues;
-        const nextNotify = getNextNotifyTime(user, task);
+        if (notificationsDone >= task.notificationsNeed) {
+            const DBUserResponse = await DB.model('User').findOne({ where: { id: task.user_id } });
+            const user = DBUserResponse.dataValues;
+            const nextNotify = getNextNotifyTime(user, task);
 
-        await DB.model('Notifies').create({ task_id: task.id, date: nextNotify.date, time: nextNotify.time });
+            await DB.model('Notifies').create({ task_id: task.id, date: nextNotify.date, time: nextNotify.time });
+        }
     }   catch (e) {
         console.log(e);
         throw new Error(`Cannot create notify! ${e.message}`);

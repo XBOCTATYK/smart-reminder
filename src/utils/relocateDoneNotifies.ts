a071100533071e4a90947e4ec96b600a04d7f5ad
XBOCTATYK <sha-ej@yandex.ru>
2020-10-22 01:05:09 +0300

    fix relocate tasks

--- src/utils/relocateDoneNotifies.ts
+++ src/utils/relocateDoneNotifies.ts
@@ -1,9 +1,10 @@
+import { v1 } from 'uuid';
 import { DONE_NOTIFIES_KEY, NOTIFICATION_ENTITY_KEY } from 'Constants/enitityNames';
 import { ORMConnection } from 'Src/db/orm-connection';
 
 export async function relocateDoneNotifies(DB: ORMConnection) {
     const doneNotifies = await DB.model(NOTIFICATION_ENTITY_KEY).findAll({ where: { done: true } });
-    const doneNotifiesValues = doneNotifies.map(item => item.dataValues);
+    const doneNotifiesValues = doneNotifies.map(item => ({ ...item.dataValues, id: v1() }));
 
     await DB.model(DONE_NOTIFIES_KEY).bulkCreate(doneNotifiesValues);
     await DB.model(NOTIFICATION_ENTITY_KEY).destroy({ where: { done: true } });
--- src/utils/relocateDoneTasks.ts
+++ src/utils/relocateDoneTasks.ts
@@ -1,9 +1,10 @@
+import { v1 } from 'uuid';
 import { DONE_TASK_KEY, TASK_ENTITY_KEY } from 'Constants/enitityNames';
 import { ORMConnection } from 'Src/db/orm-connection';
 
 export async function relocateDoneTasks(DB: ORMConnection) {
     const doneTasks = await DB.model(TASK_ENTITY_KEY).findAll({ where: { done: true } });
-    const doneTasksValues = doneTasks.map(item => item.dataValues);
+    const doneTasksValues = doneTasks.map(item => ({ ...item.dataValues, id: v1() }));
 
     await DB.model(DONE_TASK_KEY).bulkCreate(doneTasksValues);
     await DB.model(TASK_ENTITY_KEY).destroy({ where: { done: true } })

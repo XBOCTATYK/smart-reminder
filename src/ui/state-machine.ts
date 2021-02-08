773a07e2dd4c7f59277bb1d9ea7e08c80ac5609c
XBOCTATYK <sha-ej@yandex.ru>
2021-02-08 23:59:04 +0300

    feat(*): comments and filling state machine

--- src/ui/state-machine.ts
+++ src/ui/state-machine.ts
@@ -1,33 +1,81 @@
 export const STATES = {
-    PENDING_TASK: 'PENDING_TASK',
-    START: 'START',
-    FROM_TIME: 'FROM_TIME',
-    TO_TIME: 'TO_TIME',
-    ENTER_TASK_NAME: 'ENTER_TASK_NAME',
-    ENTER_TASK_PRIORITY: 'ENTER_TASK_PRIORITY',
-    ENTER_TASK_DATE: 'ENTER_TASK_DATE',
-    ENTER_TASK_TIME: 'ENTER_TASK_TIME',
-    REPEATING_TASK: 'REPEATING_TASK',
-    CREATING_TASK_ERROR: 'CREATING_TASK_ERROR',
-    POSTPONE_TASK: 'POSTPONE_TASK',
+    PENDING_TASK: 'PENDING_TASK', // state for bot waiting a new task
+    START: 'START', // user is not subscribed
+    FROM_TIME: 'FROM_TIME', // setting time after which notifications may sends
+    TO_TIME: 'TO_TIME', // setting the time after which sending notifications is not allowed
+    ENTER_TASK_NAME: 'ENTER_TASK_NAME', // bot waiting for input of task name
+    ENTER_TASK_PRIORITY: 'ENTER_TASK_PRIORITY', // bot waiting for input of task priority
+    ENTER_TASK_DATE: 'ENTER_TASK_DATE', // bot waiting for user input of task date
+    ENTER_TASK_TIME: 'ENTER_TASK_TIME', // bot waiting for input of task time
+    REPEATING_TASK: 'REPEATING_TASK', // bot waiting for input of repeating settings of task
+    CREATING_TASK_ERROR: 'CREATING_TASK_ERROR', // state when task error happens. Bot waiting for next user action
+    POSTPONE_TASK: 'POSTPONE_TASK', // ...
     DONE: 'DONE',
+    UNSUBSCRIBED: 'UNSUBSCRIBED', // the user has disabled any notifications
 } as const;
 
-const STATE_MACHINE = {
-    [STATES.PENDING_TASK]: {
-        next: [
-            STATES.ENTER_TASK_NAME,
-        ],
-        actions: {
-            [STATES.ENTER_TASK_NAME]: () => {}
-        }
-    },
+const TELEGRAM_UI_STATE_MACHINE = {
     [STATES.START]: {
         next: [
             STATES.FROM_TIME,
         ],
         actions: {
-            [STATES.FROM_TIME]: () => {}
+            [STATES.FROM_TIME]: () => undefined
+        }
+    },
+    [STATES.FROM_TIME]: {
+        next: [
+            STATES.TO_TIME,
+        ],
+        actions: {
+            [STATES.TO_TIME]: () => undefined,
+        }
+    },
+    [STATES.PENDING_TASK]: {
+        next: [
+            STATES.ENTER_TASK_NAME,
+            STATES.UNSUBSCRIBED,
+        ],
+        actions: {
+            [STATES.ENTER_TASK_NAME]: () => undefined
+        }
+    },
+    [STATES.ENTER_TASK_NAME]: {
+        next: [
+            STATES.ENTER_TASK_PRIORITY,
+            STATES.PENDING_TASK,
+        ],
+        actions: {
+            [STATES.ENTER_TASK_PRIORITY]: () => undefined,
+            [STATES.PENDING_TASK]: () => undefined,
+        }
+    },
+    [STATES.ENTER_TASK_PRIORITY]: {
+        next: [
+            STATES.ENTER_TASK_DATE,
+            STATES.PENDING_TASK,
+        ],
+        actions: {
+            [STATES.ENTER_TASK_DATE]: () => undefined,
+            [STATES.PENDING_TASK]: () => undefined,
+        }
+    },
+    [STATES.ENTER_TASK_DATE]: {
+        next: [
+            STATES.ENTER_TASK_TIME,
+            STATES.PENDING_TASK,
+        ],
+        actions: {
+            [STATES.ENTER_TASK_TIME]: () => undefined,
+            [STATES.PENDING_TASK]: () => undefined,
+        }
+    },
+    [STATES.ENTER_TASK_TIME]: {
+        next: [
+            STATES.PENDING_TASK,
+        ],
+        actions: {
+            [STATES.PENDING_TASK]: () => undefined,
         }
     }
 }

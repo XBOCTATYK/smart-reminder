9f411fc8b4ebe2985b7e90b5577fe8a5108d4ea3
XBOCTATYK <sha-ej@yandex.ru>
2021-02-23 23:40:16 +0300

    feat(*): all state classes

---
+++ src/ui/states/enter-task-priority.state.ts
@@ -0,0 +1,24 @@
+import { IUIState } from '../ui-interfaces';
+import { Shape } from '../../types/shape';
+
+export class EnterTaskPriorityState implements IUIState {
+    context: any;
+    name: string;
+
+    controls(): Shape<any> {
+        return undefined;
+    }
+
+    interact(): string {
+        return '';
+    }
+
+    onEnter(): string {
+        return '';
+    }
+
+    onLeave(): string {
+        return '';
+    }
+
+}

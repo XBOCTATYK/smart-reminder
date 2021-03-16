20a2a6e457d595d35ead8c64127773fada64e2a2
XBOCTATYK <sha-ej@yandex.ru>
2021-03-16 23:45:27 +0300

    feat(*): time matchers

---
+++ src/ui/data-transformers/enter-task-time.ts
@@ -0,0 +1,10 @@
+import { Time } from '../../utils/date-services/extended-date';
+import { Matcher } from './matcher';
+import { STATES } from '../../constants/states';
+
+const matcherFunctions = [
+    { matcher: (text: string) => !!text.match(/[\d]{1,2}:[\d]{1,2}/), transformer: (text: string) => Time.of(text).get() },
+    { matcher: (text: string) => !!text.match(/[\d]{1,2}(ч|час|часа|часов)/), transformer: (text: string) => Time.of(`${parseInt(text)}:00`).get() },
+];
+
+export const enterTaskTimeTransfomers = matcherFunctions.map( item => new Matcher(STATES.ENTER_TASK_TIME, item.matcher, item.transformer));

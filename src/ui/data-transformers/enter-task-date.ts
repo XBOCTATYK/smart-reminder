dabc371cabf25183d53ab15f3e204a1cd11c0463
XBOCTATYK <sha-ej@yandex.ru>
2021-03-23 23:11:02 +0300

    feat(*): add hour matcher

--- src/ui/data-transformers/enter-task-date.ts
+++ src/ui/data-transformers/enter-task-date.ts
@@ -3,16 +3,44 @@ import { STATES } from '../../constants/states';
 import { Matcher } from './matcher';
 
 const matcherFunctions = [
-    { matcher: (text: string) => !!text.match(/[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}/), transformer: (text: string) => ExtendedDate.of(text).get() },
-    { matcher: (text: string) => !!text.match(/Сегодня/), transformer: () => ExtendedDate.of(new Date).startOfDay().get() },
-    { matcher: (text: string) => !!text.match(/Завтра/), transformer: () => ExtendedDate.of(new Date).addDays(1).startOfDay().get() },
-    { matcher: (text: string) => !!text.match(/Послезавтра/), transformer: () => ExtendedDate.of(new Date).addDays(2).startOfDay().get() },
-    { matcher: (text: string) => !!text.match(/Послепослезавтра/), transformer: () => ExtendedDate.of(new Date).addDays(3).startOfDay().get() },
-    { matcher: (text: string) => !!text.match(/Послепослепослезавтра/), transformer: () => ExtendedDate.of(new Date).addDays(4).startOfDay().get() },
-    { matcher: (text: string) => !!text.match(/через ([\d]{1,2}) ?(день|дня|дней|деньков|денечков|дн$|д$|)+/)[2], transformer: (text: string) => {
-        const [, days] = text.match(/через ([\d]+) ?(день|дня|дней|деньков|денечков|дн$|д$|)+/);
-        return ExtendedDate.of(new Date).addDays(parseInt(days)).startOfDay().get()
-   }},
+    {
+        matcher: (text: string) => !!text.match(/[\d]{1,2}\.[\d]{1,2}\.[\d]{1,2}/),
+        transformer: (text: string) => ExtendedDate.of(text).get()
+    },
+    {
+        matcher: (text: string) => !!text.match(/Сегодня/),
+        transformer: () => ExtendedDate.of(new Date).startOfDay().get()
+    },
+    {
+        matcher: (text: string) => !!text.match(/Завтра/),
+        transformer: () => ExtendedDate.of(new Date).addDays(1).startOfDay().get()
+    },
+    {
+        matcher: (text: string) => !!text.match(/Послезавтра/),
+        transformer: () => ExtendedDate.of(new Date).addDays(2).startOfDay().get()
+    },
+    {
+        matcher: (text: string) => !!text.match(/Послепослезавтра/),
+        transformer: () => ExtendedDate.of(new Date).addDays(3).startOfDay().get()
+    },
+    {
+        matcher: (text: string) => !!text.match(/Послепослепослезавтра/),
+        transformer: () => ExtendedDate.of(new Date).addDays(4).startOfDay().get()
+    },
+    {
+        matcher: (text: string) => !!text.match(/через ([\d]{1,2}) ?(день|дня|дней|деньков|денечков|дн$|д$|)+/)[2],
+        transformer: (text: string) => {
+            const [ , days ] = text.match(/через ([\d]+) ?(день|дня|дней|деньков|денечков|дн$|д$|)+/);
+            return ExtendedDate.of(new Date).addDays(parseInt(days)).startOfDay().get()
+        }
+    },
+    {
+        matcher: (text: string) => !!text.match(/через ([\d]{1,2}) ?(час|часа|часов|часика|часиков|часик|чс$|ч$|)+/)[2],
+        transformer: (text: string) => {
+            const [ , hours ] = text.match(/через ([\d]+) ?(час|часа|часов|часика|часиков|часик|чс$|ч$|)+/);
+            return ExtendedDate.of(new Date).addHours(parseInt(hours)).startOfDay().get()
+        }
+    }
 ];
 
-export const enterTaskDataMatchers = matcherFunctions.map( item => new Matcher(STATES.ENTER_TASK_DATE, item.matcher, item.transformer));
+export const enterTaskDataMatchers = matcherFunctions.map(item => new Matcher(STATES.ENTER_TASK_DATE, item.matcher, item.transformer));

60121924a01f0c4f87d7a34ffff810d07b30eb50
XBOCTATYK <sha-ej@yandex.ru>
2021-04-01 23:46:46 +0300

    feat(*): added matcher

--- src/ui/data-transformers/pending-task.ts
+++ src/ui/data-transformers/pending-task.ts
@@ -2,7 +2,10 @@ import { Matcher } from './matcher';
 import { STATES } from '../../constants/states';
 
 const matcherFunctions = [
-
+    {
+        matcher: (text: string) => !!text.match(/Добавить/),
+        transformer: () => 'ADD'
+    }
 ]
 
 export const enterTaskDataMatchers = matcherFunctions.map(item => new Matcher(STATES.PENDING_TASK, item.matcher, item.transformer));
